4c3f48a582e5c807a361a0f63e5085f536e49b15
tahmid1234 <tahmidislam1998@gmail.com>
2020-12-22 10:40:12 +0600

    Intro of recipient added

---
+++ src/sample/controllers/InfoOfRecipentController.java
@@ -0,0 +1,30 @@
+package sample.controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/*public class InfoOfRecipentController implements Initializable {
+    @FXML
+    private Label recipienName;
+    @FXML private Label recipientBloodGroup;
+    @FXML private Label recipientContct;
+
+
+            recipienName.setText(SearchingForRecipent.recipentName);
+            recipientContct.setText(SearchingForRecipent.recipenContact);
+            recipientBloodGroup.setText(SearchingForRecipent.recipentBloodGroup);
+
+
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+
+    }
+}*/
