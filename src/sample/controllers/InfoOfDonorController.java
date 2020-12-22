520acb615c3792732065e6642485c372eea3b1b4
tahmid1234 <tahmidislam1998@gmail.com>
2020-12-22 10:38:53 +0600

    Intro of donor added

---
+++ src/sample/controllers/InfoOfDonorController.java
@@ -0,0 +1,18 @@
+package sample.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class InfoOfDonorController {
+    @FXML private Label recipientName;
+    @FXML private Label recipientBloodGroup;
+    @FXML private Label recipientContct;
+
+    public void settingInfo(){
+
+
+
+    }
+
+
+}
